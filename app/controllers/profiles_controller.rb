8a21865f73565d7f67d7d31cfb5e4cce85182bb2
jcbridwe <jcbridwe@gmail.com>
2016-10-27 17:51:35 +0000

    Added form partial, and edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,8 @@
 class ProfilesController < ApplicationController
     def new
-       # form where each user can fill out their own profile.
-       @user = User.find( params[:user_id] )
-       @profile = Profile.new
+   # form where each user can fill out their own profile.
+   @user = User.find( params[:user_id] )
+   @profile = Profile.new
     end
     def create
        @user = User.find( params[:user_id] )
@@ -14,6 +14,11 @@ class ProfilesController < ApplicationController
           render action: :new
        end
     end
+    def edit
+       @user = User.find( params[:user_id] )
+       @profile = @user.profile
+    end
+    
     private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
